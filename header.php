1de0164ce2c5fbf82fc0167b8dd18eba0063ecb9
Ajay Kumar <31202856+Ajay750000@users.noreply.github.com>
2020-08-25 14:03:47 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php wp_head(); ?>
+	
 	 <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Go Tours</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <?php wp_head();?>
+        <title><?php the_title();?></title>
 </head>
 <body>
 
